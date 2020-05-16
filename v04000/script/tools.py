04b0f821a2bd150ce897e3126c068f9fad41e852
konumaru <konumaru1022@gmail.com>
2020-05-17 02:07:22 +0900

    [update] run v04000.

---
+++ v04000/script/tools.py
@@ -0,0 +1,65 @@
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+
+
+def load_pickle(filepath):
+    with open(filepath, 'rb') as file:
+        return pickle.load(file)
+
+
+def dump_pickle(data, filepath):
+    with open(filepath, 'wb') as file:
+        pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def cache_result(filename, use_cache=True):
+    def _acept_func(func):
+        def run_func(*args, **kwargs):
+            filepath = f'features/{filename}.pkl'
+            if use_cache and os.path.exists(filepath):
+                print(f'Load Cached data, {filepath}')
+                return load_pickle(filepath)
+            result = func(*args, **kwargs)
+
+            print(f'Cache to {filepath}')
+            dump_pickle(result, filepath)
+            return result
+        return run_func
+    return _acept_func
+
+
+def reduce_mem_usage(df, verbose=True):
+    numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    if verbose:
+        print(
+            "Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)".format(
+                end_mem, 100 * (start_mem - end_mem) / start_mem
+            )
+        )
+    return df
--- v04000/version_ reference.md
+++ v04000/version_ reference.md
@@ -1,1 +1,25 @@
 # Version Reference
+
+v03000 で Baseline を決めることができた。
+ひとまずの目的関数と評価関数を固めたが、目的関数には不安が残った。
+
+一方で特徴量に未着手なので、v04000で一旦、特徴量エンジニアリングを行う
+
+
+## Plan
+
+- price や calendar の特徴量を追加
+  - https://www.kaggle.com/kyakovlev/m5-simple-fe
+- lag 特徴量を追加
+  - https://www.kaggle.com/kyakovlev/m5-lags-features
+- custom features
+  - https://www.kaggle.com/kyakovlev/m5-custom-features
+
+
+## Experiments
+### v04000
+- Copied from v03004
+
+
+### v04001:
+-
